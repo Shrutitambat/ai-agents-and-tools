4e8b3c89e23c2f7dec27b1a2936cc724eabf7c71
Shrutitambat <149862790+Shrutitambat@users.noreply.github.com>
2026-06-03 14:21:20 +0530

    Implemented tool calling and news summarization

---
+++ toolcalling.py
@@ -0,0 +1,55 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_mistralai import ChatMistralAI
+from langchain.tools import tool 
+from langchain_core.messages import HumanMessage
+from rich import print 
+
+#1 creating a tool 
+
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the number of character in a given text"""
+    return len(text)
+
+tools = {
+    "get_text_length" : get_text_length
+}
+
+
+llm = ChatMistralAI(model = "mistral-small-2506")
+
+#tool binding 
+llm_with_tool = llm.bind_tools([get_text_length])
+
+
+##Just binding the tools 
+#result = llm_with_tool.invoke("Hello")
+#result2 = llm.invoke("Hello")
+
+##Now calling the tools
+#result = llm_with_tool.invoke("Returns the number of character in a given text:'how are you'")
+#result2 = llm.invoke("Returns the number of character in a given text:'how are you'")
+
+##tool  execution
+
+message = []
+prompt = input("You: ")
+query = HumanMessage(prompt)
+message.append(query)
+
+result = llm_with_tool.invoke(message)
+
+message.append(result)
+
+if result.tool_calls:
+    tool_name = result.tool_calls[0]["name"]
+    tool_message = tools[tool_name].invoke(result.tool_calls[0])
+    message.append(tool_message)
+   
+
+result = llm_with_tool.invoke(message)
+print(result.content)
+
+#print(result)
+#print(result2)
